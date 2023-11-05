3c7605d2807d659bf9b9517c4a7ef9beaa5dd06f
James Newton <blackslate@lexogram.com>
2023-11-05 13:09:55 +0300

    Improve and comment solutions
    * Increase spacing between tasks
    * Lay out the activities object more neatly
    * scrambleWords
      + Invert how `punctuation` is set
      + Add general comments
      + Add specific comments on how Array.join() treats undefined
    * convertNestedArrayToObject
      + Destructure keys and values directly in the parameter
      + Remove unnecessary square brackets around [key]
    * makeGameGrid
      + Rename output variable
      + Use basic for loops instead of Array.map
    * generateAverages
      + Use reducer to explicitly calculate daily averages
      + Make explicit the use of a side effect to prepare for the overall average
      + Rename parameter and some variables
    * StaffMember
      + Use ternary operator to simplify calculation in presentValue
      + Apply DRY principles to the getStatus() method
      + Call this.getStatus() and this.getValue outside the generation of the message
      + Save the message in a variable before returning it
    * validateName
      + Use a space rather than a regex to split into words
      + Replace non-breaking spaces with standard spaces
      + Remove unnecessary `trim()` and `last` lines
      + Improve layout of multi-criteria boolean expression

--- solution.js
+++ solution.js
@@ -1,43 +1,56 @@
 // Answer Sheet - all answers should go here 
 
 /* 
-except for A-1 and C, you will always write a function that returns values
-
+Except for A-1 and C, you will always write a function that returns values
 */
+
 
 /* A-1
 define and fill an object */
 const activities = {
-  "Monday": "Swimming",
-  "Tuesday": "Book Club",
-  "Wednesday":"Gymnastics",
-  "Thursday": "Babysitting",
-  "Friday": "Debating Society"
+  "Monday"    : "Swimming",
+  "Tuesday"   : "Book Club",
+  "Wednesday" :"Gymnastics",
+  "Thursday"  : "Babysitting",
+  "Friday"    : "Debating Society"
 };
+
 
 /* A-2 */
 function createNestedArrayFromObject(object) {
   return Object.entries(object)
 }
 
+
 /* B-1 */
 const scrambleWords = (sentence) => {
-  let punctuation = ""
+  let punctuation = sentence.substr(-1)
 
-  if (".!?".includes(sentence.substr(-1))) {
-    punctuation = sentence.substr(-1)
+  if (".!?".includes(punctuation)) {
     sentence = sentence.slice(0, -1)
+  } else {
+    punctuation = ""
   }
   
-  const words = sentence.split(/\s+/)
+  const words = sentence.split(" ")
+
   const scrambled = words.map(word => {
-    let letters = [...word]
+    let letters = [...word] // same as word.split("")
     letters = letters.reverse()
-    const first = letters.pop()
-    letters.push(letters.shift())
+    // Take what is now the last letter,...
+    const first = letters.pop() // letters _could_ be []
+    // ... place what is now the first letter at the end
+    letters.push(letters.shift()) // letters _could_ be [undefined]
+    // ... and insert the popped letter at the beginning
     letters.unshift(first)
 
-    return letters.join("")
+    return letters.join("") // ["a", undefined].join("") is "a"
+
+    // https://262.ecma-international.org/5.1/#sec-15.4.4.5
+    // 15.4.4.5Array.prototype.join (separator)
+    // 8. If element0 is undefined or null,
+    //    let R be the empty String;
+    //    otherwise, Let R be ToString(element0).
   })
 
   return scrambled.join(" ") + punctuation
@@ -45,11 +58,10 @@ const scrambleWords = (sentence) => {
 
 
 /* B-2 */
-function convertNestedArrayToObject(nestedArray) {
-  const [ keys, values ] = nestedArray
+function convertNestedArrayToObject([ keys, values ]) {
 
   const reducer = ( object, key, index ) => {
-    object[[key]] = values[index]
+    object[key] = values[index]
     return object
   }
 
@@ -59,50 +71,50 @@ function convertNestedArrayToObject(nestedArray) {
 
 /* B-3 */
 const makeGameGrid = ({ rows, columns }) => {
-  const rowArray = new Array(rows).fill(0)
-  const grid = rowArray.map((_, row) => {
-    const columnArray = new Array(columns).fill(0)
-    return columnArray.map((_, column) => {
-      return `r${row + 1}c${column + 1}`
-    })
-  })
+  const gameGrid = []
+  
+  for ( let row = 1; row <= rows; row += 1 ) {
+    const newRow = []
+    gameGrid.push(newRow)
 
-  return grid
+    for ( let column = 1; column <= columns; column += 1 ) {
+      newRow.push(`r${row}c${column}`)
+    }
+  }
+  
+  return gameGrid
 };
 
 
 /* B-4 */
-const generateAverages = (object) => {
-  const entries = Object.entries(object)
-  // [[ "Day 1", [12, 14, 52, 37, 5]], ...]
+const generateAverages = (data) => {
+  const entries = Object.entries(data)
 
-  const reducer = (accumulator, [day, data]) => {
-    let { averages, total, count } = accumulator
+  let total = 0
+  let count = 0
 
-    const readings = data.length
-    const sum = data.reduce((total, reading) => {
-      return total + reading
-    }, 0)
+  const sum = array => array.reduce(
+    (total, number) => total + number
+  )
 
-    total += sum
-    count += readings
+  const reducer = ( accumulator, [day, values] ) => {
+    const subTotal = sum(values)
+    const length = values.length
+    accumulator[day] = subTotal / length
 
-    averages[day] = sum / readings
+    // <<< Side effects
+    total += subTotal
+    count += length
+    // Side effects >>>
 
-    return {
-      averages,
-      total,
-      count
-    }
+    return accumulator
   }
-  
-  accumulator = { averages: {}, total: 0, count: 0}
-  const result = entries.reduce(reducer, accumulator)
 
-  const { averages, total, count } = result
-  averages["Overall"] = total / count
+  const output = entries.reduce(reducer, {})
 
-  return averages
+  output["Overall"] = total / count
+
+  return output
 };
 
 
@@ -117,46 +129,54 @@ class StaffMember{
   }
 
   presentValue() {
-    let value = 25
-    if (this.partner_name) {
-      value += 7
-    }
-    value += this.number_of_children * 5
+    const value = 25
+                + (this.partner_name ? 7 : 0)
+                + (this.number_of_children * 5)
 
     return value
   }
 
   getStatus() {
-    let status = ""
-    if (this.partner_name) {
-      status += `${this.first_name} has a partner called ${this.partner_name}`
-      if (this.number_of_children) {
-        status += ( this.number_of_children === 1 )
-                  ? ", and 1 child. "
-                  : `, and ${this.number_of_children} children. `
-      } else {
-        status += ". "
+    let status = " "
+
+    if (this.partner_name || this.number_of_children) {
+      status = ` ${this.first_name} has `
+
+      if (this.partner_name) {
+        status += `a partner called ${this.partner_name}`
+        if (this.number_of_children) {
+          status += `, and `
+        }
       }
-    } else if (this.number_of_children) {
-      status += ( this.number_of_children === 1 )
-                ? `${this.first_name} has 1 child. `
-                : `${this.first_name} has ${this.number_of_children} children. `
+
+      if (this.number_of_children === 1) {
+        status += `1 child`
+      } else if (this.number_of_children) {
+        status += `${this.number_of_children} children`
+      }
+    
+      status += ". "
     }
 
-    return status
+    return status 
   }
 
   getMessage() {
-    return `Please buy a present for ${this.first_name} ${this.family_name}. ${this.getStatus()}Pyramid Sales will reimburse your purchase up to a maximum value of €${this.presentValue()}.`
+    const status = this.getStatus()
+    const value = this.presentValue()
+
+    const message = `Please buy a present for ${this.first_name} ${this.family_name}.${status}Pyramid Sales will reimburse your purchase up to a maximum value of €${value}.`
+
+    return message
   }
 };
 
 
 /* D */
 const validateName = (name) => {
-  const words = name.split(/\s+/)
+  const words = name.split(" ")
   const wordCount = words.length
-  if (wordCount < 2 || wordCount > 3) {
+  if (wordCount < 2 || wordCount > 3) {
     return false
   }
 
@@ -172,11 +192,9 @@ const validateName = (name) => {
   }
  
   let isValid = words.every( word => {
-    word = word.trim()
     const length = word.length
     const initial = word[0]
     const rest = word.substring(1)
-    const last = word[length - 1]
 
     if (initial === ".") {
       // Words cannot start with a dot
@@ -184,7 +202,7 @@ const validateName = (name) => {
     } else if (length < 2) {
       // Initials must be followed by a dot
       return false
-    } else if (initial !== initial.toUpperCase()) {
+    } else if (initial !== initial.toUpperCase()) {      
       // Initial letters must be capitals
       return false
     } else if (rest !== rest.toLowerCase()) {
@@ -205,10 +223,11 @@ const validateName = (name) => {
     return false
   }
 
-  if ( words[0][1] === "."      // first word is an initial
+  if (
+       words[0][1] === "."      // first word is an initial
     && wordCount === 3          // there are three words
     && !words[1].includes(".")  // middle name is not an initial
-    ) {
+  ) {
     // Middle name cannot be expanded if first is an initial
     return false
   }
@@ -221,6 +240,7 @@ const validateName = (name) => {
 
   return true
 };
+
 
 // Do not edit or answer below this line
 // EDITING MODULE EXPORTS WILL RESULT IN AN AUTOMATIC FAIL
