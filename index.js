8df349c680cad0e537054553e6cd4922a92e4844
James Newton <blackslate@lexogram.com>
2023-11-05 12:32:35 +0300

    Cosmetic changes to index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 // Answer Sheet - all answers should go here 
 
 /* 
-except for A-1 and C, you will always write a function that returns values
-
+Except for A-1 and C, you will always write a function that returns values
 */
 
 /* A-1
 define and fill an object */
 const activities = null;
 
+
 /* A-2 */
 function createNestedArrayFromObject() {}
+
 
 /* B-1 */
 const scrambleWords = null;
 
+
 /* B-2 */
 function convertNestedArrayToObject() {};
 
+
 /* B-3 */
 const makeGameGrid = null;
+
 
 /* B-4 */
 const generateAverages = null;
@@ -32,6 +36,7 @@ class StaffMember{};
 
 /* D */
 const validateName = null;
+
 
 // Do not edit or answer below this line
 // EDITING MODULE EXPORTS WILL RESULT IN AN AUTOMATIC FAIL
